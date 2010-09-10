9dfba356e1567352d328f35ca4d528fb5c0b1799
Shawn Allison <shawn@yayinternets.com>
2010-09-10 14:52:50 -0400

    1.5.12 - added mixin for css3 box-sizing and added said mixin to reset lib file for textareas. WOAH

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ require "bundler"
 
 # START THIS THING
 puts  "\n========================================================="
-puts  " SHAWN'S RAILS 3 TEMPLATE - [v1.5.11] ".yellow.bold
+puts  " SHAWN'S RAILS 3 TEMPLATE - [v1.5.12] ".yellow.bold
 puts  "=========================================================\n\n"
 
 # REMOVE FILES
