d991d0ec817b83e8cb2664c90ad632181ed8322e
Hampton Catlin <hcatlin@gmail.com>
2010-10-12 14:31:57 +0100

    Turning on line-numbering for Sass while in development mode.

--- templates/plugins.rb
+++ templates/plugins.rb
@@ -1,4 +1,10 @@
 Haml::Template.options[:attr_wrapper]     = '"'
-Sass::Plugin.options[:style]              = :compressed
+if Rails.env == "development"
+  Sass::Plugin.options[:line_numbers]     = true
+  Sass::Plugin.options[:style]            = :nested
+else
+  Sass::Plugin.options[:style]            = :compressed
+end
+Sass::Plugin.options[:syntax]             = :scss
 Sass::Plugin.options[:template_location]  = (Rails.root + 'app' + 'sass').to_s
 Sass::Plugin.options[:css_location]       = (Rails.root + 'public' + 'stylesheets').to_s
